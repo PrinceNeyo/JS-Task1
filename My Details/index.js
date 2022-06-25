e988a2f16b5509b7a310fd6698622af69e618938
PrinceNeyo <105304385+PrinceNeyo@users.noreply.github.com>
2022-06-25 20:08:09 +0800

    Add files via upload

---
+++ My Details/index.js
@@ -0,0 +1,3 @@
+document.getElementById("name").innerHTML = "Hello my name is ADEBAYO Adeniyi";
+document.getElementById("height").innerHTML = "I am 1.70m tall";
+document.getElementById("country").innerHTML = "I am from Nigeria";
